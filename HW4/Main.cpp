04a25b0cf9214d045cd89986cfc11db936e9a78d
Halcyon <90397168+USHalcyon@users.noreply.github.com>
2022-12-31 13:42:20 +0700

    Add files via upload

---
+++ HW4/Main.cpp
@@ -0,0 +1,48 @@
+#include "node.h"
+
+int main() {
+	NODE* n = createNode(9);
+
+	List* l = createList(n);
+	cout << "Current list:\n";
+	printList(l);
+	cout << "addHead(1)\n";
+	addHead(l, 1);
+	printList(l);
+	addHead(l, 3);
+	addHead(l, 4);
+	addHead(l, 2);
+	addHead(l, 5);
+	cout << "After addHead() a few numbers:\n";
+	printList(l);
+	addTail(l, 6);
+	cout << "After addTail(6):\n";
+	printList(l);
+	cout << "After removeHead(l)\n";
+	removeHead(l);
+	printList(l);
+	cout << "After removeTail(l)\n";
+	removeTail(l);
+	printList(l);
+	cout << "After addAfter(l, 15, 3) - add 15 after 3\n";
+	addAfter(l, 15, 3);
+	printList(l);
+	cout << "After addAfter(l, 16, 11) - add 16 after 11\n";
+	cout << "As 11 does not exist, do nothing\n";
+	addAfter(l, 16, 11);
+	printList(l);
+
+	cout << "After removeAfter(l, 15) - remove the number after 15\n";
+	removeAfter(l, 15);
+	printList(l);
+	cout << "After removeAfter(l, 13) - remove the number after 13\n";
+	cout << "As 13 does not exist, do nothing\n";
+	removeAfter(l, 13);
+	printList(l);
+
+	// Release memory
+	while (l->p_head)
+		removeHead(l);
+
+	return 0;
+}
