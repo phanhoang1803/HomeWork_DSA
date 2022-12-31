47e5adc09831c6d2bf646b0b72ab29c310d9b309
f1amingboyant <121161978+f1amingboyant@users.noreply.github.com>
2022-12-31 12:19:41 +0700

    Update CircularLinkedlist.cpp

--- HW4/CircularLinkedlist.cpp
+++ HW4/CircularLinkedlist.cpp
@@ -4,7 +4,7 @@ int main() {
 	NODE* n = createNode(9);
 
 	List* l = createList(n);
-	cout << "List hien tai:\n";
+	cout << "Current list:\n";
 	printList(l);
 	cout << "addHead(1)\n";
 	addHead(l, 1);
@@ -13,30 +13,30 @@ int main() {
 	addHead(l, 4);
 	addHead(l, 2);
 	addHead(l, 5);
-	cout << "Sau khi add Head 1 vai so:\n";
+	cout << "After addHead() a few numbers:\n";
 	printList(l);
 	addTail(l, 6);
-	cout << "Sau khi addTail(6):\n";
+	cout << "After addTail(6):\n";
 	printList(l);
-	cout << "Sau khi removeHead(l)\n";
+	cout << "After removeHead(l)\n";
 	removeHead(l);
 	printList(l);
-	cout << "Sau khi removeTail(l)\n";
+	cout << "After removeTail(l)\n";
 	removeTail(l);
 	printList(l);
-	cout << "Sau khi addAfter(l, 15, 3), them 15 sau 3\n";
+	cout << "After addAfter(l, 15, 3) - add 15 after 3\n";
 	addAfter(l, 15, 3);
 	printList(l);
-	cout << "Sau khi addAfter(l, 16, 11), them 16 sau 11\n";
-	cout << "Vi khong co 11 nen khong lam gi\n";
+	cout << "After addAfter(l, 16, 11) - add 16 after 11\n";
+	cout << "As 11 does not exist, do nothing\n";
 	addAfter(l, 16, 11);
 	printList(l);
 
-	cout << "Sau khi removeAfter(l, 15), xoa so sau 15\n";
+	cout << "After removeAfter(l, 15) - remove the number after 15\n";
 	removeAfter(l, 15);
 	printList(l);
-	cout << "Sau khi removeAfter(l, 13), xoa so sau 13\n";
-	cout << "Vi khong co 13 nen khong lam gi\n";
+	cout << "After removeAfter(l, 13) - remove the number after 13\n";
+	cout << "As 13 does not exist, do nothing\n";
 	removeAfter(l, 13);
 	printList(l);
 
